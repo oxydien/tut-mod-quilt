ff5cda7253e4014247dc391dbe37692510294823
oxydien <olda.talian@pm.me>
2023-07-05 16:48:10 +0200

    Jednoduchy item

--- src/main/java/dev/oxydien/tut_mod/TutModInit.java
+++ src/main/java/dev/oxydien/tut_mod/TutModInit.java
@@ -1,5 +1,13 @@
 package dev.oxydien.tut_mod;
 
+import dev.oxydien.tut_mod.items.ModItems;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
@@ -8,12 +16,17 @@ import org.slf4j.LoggerFactory;
 public class TutModInit implements ModInitializer {
 	public static final String MOD_ID = "tut_mod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final ItemGroup TutModGroup = FabricItemGroup.builder()
+		.name(Text.translatable("itemgroup.tut_mod.group"))
+		.icon(() -> new ItemStack(ModItems.URANIUM)).build();
 	public static void Log(String message, Object... objects) {
 		LOGGER.info("<TUT_MOD>: " + message,objects);
 	}
 
 	@Override
 	public void onInitialize(ModContainer mod) {
-		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+		Registry.register(Registries.ITEM_GROUP, new Identifier(MOD_ID,"tut_mod_group"),TutModGroup);
+
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/dev/oxydien/tut_mod/items/ModItems.java
@@ -0,0 +1,36 @@
+package dev.oxydien.tut_mod.items;
+
+import dev.oxydien.tut_mod.TutModInit;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
+
+public class ModItems {
+	public static final Item URANIUM = registerItem("uranium",
+		new Item(new QuiltItemSettings()));
+	public static final Item RAW_URANIUM = registerItem("raw_uranium",
+		new Item(new QuiltItemSettings()));
+
+	private static Item registerItem(String name, Item item) {
+		Item NITEM = Registry.register(Registries.ITEM, new Identifier(TutModInit.MOD_ID, name), item);
+		AddItemToGroup(NITEM);
+		return NITEM;
+	}
+
+	private static void AddItemToGroup(Item item) {
+		ItemGroupEvents.modifyEntriesEvent(RegistryKey.of(RegistryKeys.ITEM_GROUP,
+			new Identifier(TutModInit.MOD_ID, "tut_mod_group")))
+			.register(entries -> {
+				entries.addItem(item);
+			});
+	}
+
+	public static void registerModItems() {
+		TutModInit.Log("Registruji itemy");
+	}
+}
